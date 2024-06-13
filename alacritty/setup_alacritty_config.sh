147cab097ad44c420db19af91626f8b1788d59f2
João Tribuzy <J169866@SA0099975.local>
2024-06-13 15:34:14 -0400

    feat: copy alacritty configuration to system .config folder

---
+++ alacritty/setup_alacritty_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Source file path
+source_file="alacritty.toml"
+
+# Destination directory
+destination_dir="$HOME/.config/alacritty"
+
+# Destination file
+destination_file="$destination_dir/alacritty.toml"
+
+# Check if the source file exists
+if [ ! -f "$source_file" ]; then
+    echo "Source file not found: $source_file"
+    exit 1
+fi
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Copy the content of the source file to the destination file
+cp "$source_file" "$destination_file" && echo "Content copied successfully to $destination_file"
