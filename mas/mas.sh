d355e8dd667fc9a1d7e370ed47263e7e497b6441
João Tribuzy <J169866@SA0099975.local>
2024-06-14 11:15:54 -0400

    feat: run xcodes.sh from /xcode to select xcode version on xcodes

--- mas/mas.sh
+++ mas/mas.sh
@@ -6,7 +6,10 @@ echo
 mas install 1091189122
 
 # Spark Mail Client
-mas install 1176895641
+mas install 6445813049
 
 # Trello
 mas install 1278508951
+
+# Xcode
+mas install 497799835
---
+++ xcodes/xcodes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+echo "--------- 🔨 XCODE ---------"
+echo
+
+# Find the path to any application that has "Xcode" in its name
+xcode_app_path=~/Applications/Xcode.app
+
+# Make the installed Xcode active
+echo "Making Xcode active..."
+xcodes select "$xcode_app_path"
+
+echo "Xcode latest version installation and setup complete."
